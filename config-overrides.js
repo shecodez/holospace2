5344b15b31f63a124b353bf72beea08780418fa7
shecodez - Nicole J. Nobles <shecodez@gmail.com>
2018-05-27 14:25:45 -0400

    Add Ant Design, rewired, and SASS support

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+	config = injectBabelPlugin(
+		['import', { libraryName: 'antd', style: true }],
+		config
+	);
+	config = rewireLess.withLoaderOptions({
+		modifyVars: {
+			'@primary-color': '#6435c9'
+		}
+	})(config, env);
+	return config;
+};
